ec1ba5b270986d82ba82372c3cdce4bcdcbfc9c0
Erik <epstorrs@gmail.com>
2022-02-22 11:16:52 -0600

    added example run

--- examples/ht191/create_example_run.sh
+++ examples/ht191/create_example_run.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-python ../../src/generate_run_commands.py --sequencing-info  sequencing_info.txt run_list.txt example_run
+python ../../src/compute1/generate_run_commands.py --sequencing-info  sequencing_info.txt run_list.txt /storage1/fs1/dinglab/Active/Projects/estorrs/pecgs-pipeline/examples/ht191/example_run
+
+# if running this script from a non-compute1 system you can set a proxy run directory where the run directory will be written. You can then copy this directory to compute1 and start the run
+# python ../../src/compute1/generate_run_commands.py --proxy-run-dir example_run --sequencing-info  sequencing_info.txt run_list.txt /storage1/fs1/dinglab/Active/Projects/estorrs/pecgs-pipeline/examples/ht191/example_run
