cfc5f38258dda186cd65f1523588bebc552d685d
Erik <epstorrs@gmail.com>
2022-02-23 10:46:24 -0600

    generate commands working off bsub

--- examples/ht191/summarize_example_run.sh
+++ examples/ht191/summarize_example_run.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+export LSF_DOCKER_VOLUMES=/storage1/fs1/dinglab/Active/Projects/estorrs/pecgs-pipeline/examples/ht191/example_run
+export PATH=/miniconda/envs/pecgs/bin:$PATH
 
-python ../../src/compute1/generate_run_commands.py summarize-run pecgs_TN_wxs_fq_T_rna_fq run_list.txt /storage1/fs1/dinglab/Active/Projects/estorrs/pecgs-pipeline/examples/ht191/example_run
+bsub -q dinglab-interactive -G compute-dinglab -a 'docker(estorrs/pecgs-pipeline:0.0.1)' 'python ../../src/compute1/generate_run_commands.py summarize-run pecgs_TN_wxs_fq_T_rna_fq run_list.txt /storage1/fs1/dinglab/Active/Projects/estorrs/pecgs-pipeline/examples/ht191/example_run'
