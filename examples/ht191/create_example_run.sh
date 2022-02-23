719167c5270cfada3fc7659e62179919b6062faa
Erik <epstorrs@gmail.com>
2022-02-23 10:54:13 -0600

    update exports

--- examples/ht191/create_example_run.sh
+++ examples/ht191/create_example_run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-export LSF_DOCKER_VOLUMES=/storage1/fs1/dinglab/Active:/storage1/fs1/dinglab/Active /scratch1/fs1/dinglab:/scratch1/fs1/dinglab
-export PATH=/miniconda/envs/pecgs/bin:$PATH
+export LSF_DOCKER_VOLUMES="/storage1/fs1/dinglab/Active:/storage1/fs1/dinglab/Active /scratch1/fs1/dinglab:/scratch1/fs1/dinglab"
+export PATH="/miniconda/envs/pecgs/bin:$PATH"
 bsub -q dinglab-interactive -G compute-dinglab -a 'docker(estorrs/pecgs-pipeline:0.0.1)' 'python ../../src/compute1/generate_run_commands.py make-run --sequencing-info  sequencing_info.txt pecgs_TN_wxs_fq_T_rna_fq run_list.txt /storage1/fs1/dinglab/Active/Projects/estorrs/pecgs-pipeline/examples/ht191/example_run'
 
 # if running this script from a non-compute1 system you can set a proxy run directory where the run directory will be written. You can then copy this directory to compute1 and start the run
--- examples/ht191/summarize_example_run.sh
+++ examples/ht191/summarize_example_run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-export LSF_DOCKER_VOLUMES=/storage1/fs1/dinglab/Active:/storage1/fs1/dinglab/Active /scratch1/fs1/dinglab:/scratch1/fs1/dinglab
-export PATH=/miniconda/envs/pecgs/bin:$PATH
+export LSF_DOCKER_VOLUMES="/storage1/fs1/dinglab/Active:/storage1/fs1/dinglab/Active /scratch1/fs1/dinglab:/scratch1/fs1/dinglab"
+export PATH="/miniconda/envs/pecgs/bin:$PATH"
 
 bsub -q dinglab-interactive -G compute-dinglab -a 'docker(estorrs/pecgs-pipeline:0.0.1)' 'python ../../src/compute1/generate_run_commands.py summarize-run pecgs_TN_wxs_fq_T_rna_fq run_list.txt /storage1/fs1/dinglab/Active/Projects/estorrs/pecgs-pipeline/examples/ht191/example_run'
