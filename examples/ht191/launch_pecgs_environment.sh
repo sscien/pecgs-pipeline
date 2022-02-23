bf940079feabdd8a343cf0873ddd4aac16146fde
Erik <epstorrs@gmail.com>
2022-02-23 11:26:49 -0600

    make interactive

--- examples/ht191/launch_pecgs_environment.sh
+++ examples/ht191/launch_pecgs_environment.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 export LSF_DOCKER_VOLUMES="/storage1/fs1/dinglab/Active:/storage1/fs1/dinglab/Active /scratch1/fs1/dinglab:/scratch1/fs1/dinglab"
 export PATH="/miniconda/envs/pecgs/bin:$PATH"
-bsub -q dinglab-interactive -G compute-dinglab -a 'docker(estorrs/pecgs-pipeline:0.0.1)' '/bin/bash'
+bsub -q dinglab-interactive -G compute-dinglab -Is -a 'docker(estorrs/pecgs-pipeline:0.0.1)' '/bin/bash'
