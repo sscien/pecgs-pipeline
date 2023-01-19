04c73cd2a0756da6751ea2ee26e3c3b55a2c71ce
Erik Storrs <epstorrs@gmail.com>
2023-01-19 17:09:23 -0600

    fixing examples

--- examples/pecgs_TN_wgs_bam/2.tidy_run.sh
+++ examples/pecgs_TN_wgs_bam/2.tidy_run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python ../../src/compute1/generate_run_commands.py tidy-run pecgs_TN_wgs_bam run_list.txt /scratch1/fs1/dinglab/estorrs/cromwell-data/pecgs/testing/pecgs_TN_wgs_bam
+python /storage1/fs1/dinglab/Active/Projects/estorrs/pecgs-pipeline/src/compute1/generate_run_commands.py tidy-run pecgs_TN_wgs_bam run_list.txt /scratch1/fs1/dinglab/estorrs/cromwell-data/pecgs/testing/pecgs_TN_wgs_bam
